7dc91d602bdf847e3f8e2e37c01ff9593c822ef3
sergioaguirrenatura <sergioaguirre@natura.net>
2026-08-19 11:03:15 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# Importar las librerías instaladas
+import pandas as pd
+import streamlit as st
+import numpy as np
+import plotly.express as px
+
+# Importar BytesIO (que ya viene preinstalado en Python)
+from io import BytesIO
+
+st.title("¡Mi entorno está configurado correctamente!")
+st.write("Si puedes ver esto, instalaste todo con éxito.")
